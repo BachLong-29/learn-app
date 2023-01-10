1c0cfdf6901c803aa69fc5d8827409299fea9db6
BachLong-29 <bachthanhlong0821@gmail.com>
2023-01-10 21:49:42 +0700

    change todo page

--- components/TodoPage/Todo.tsx
+++ components/TodoPage/Todo.tsx
@@ -1,22 +1,16 @@
-import Computer from './components/Computer';
-import Keyboard from './components/Keyboard';
-import React from 'react';
+import Image from 'next/image';
 import styled from 'styled-components';
 import styles from './styles/Todo.module.css';
 
 const CommingSoon = styled.div`
-  position: absolute;
-  top: 35%;
-  right: 25%;
   color: #f42c41;
-  font-size: 39px;
+  font-size: 60px;
 `;
 
 const Todo = () => {
   return (
     <div className={styles['Todo-container']}>
-      <Computer />
-      <Keyboard />
+      <Image src='/images/Computer.png' alt='abcc' width={500} height={500}/>
       <CommingSoon>
         <div>Comming</div>
         <div>soon</div>

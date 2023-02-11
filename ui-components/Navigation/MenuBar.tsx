787a6a6c04aaa78773254c0bcc311ae35785b248
BachLong-29 <bachthanhlong0821@gmail.com>
2023-02-11 22:34:54 +0700

    responsive for menu bar

--- ui-components/Navigation/MenuBar.tsx
+++ ui-components/Navigation/MenuBar.tsx
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import styles from './styles/MenuBar.module.css';
 import { useRouter } from 'next/router';
 
 const MenuBar = () => {
   const router = useRouter();
-  const toggleMenu = () => {
-    const menuToggle = document.querySelector('.menuToggle');
-    const navigation = document.querySelector('.navigation');
-    menuToggle?.classList.toggle('active');
-    navigation?.classList.toggle('active');
-  };
+  const [scroll, setScroll] = useState(false);
+  const [active, setActive] = useState(false);
+  
   const goHome = () => {
     router.push('/');
+    setActive(false);
   };
   const goStudentBoard = () => {
     router.push('/student');
+    setActive(false);
   };
   const goClassBoard = () => {
     router.push('/class');
+    setActive(false);
   };
   const goExamBoard = () => {
     router.push('/exam');
+    setActive(false);
   };
+  
+  useEffect(() => {
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 0) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    });
+  }, [scroll]);
+
+  const toggleMenu = () => {
+    setActive(!active);
+  };
+
   return (
     <>
       <div className={styles.containerA}>
-        <div className={styles.header}>
-          <div className={styles.menuToggle} onClick={toggleMenu}></div>
-          <ul className={styles.navigation}>
+        <div
+          className={`${styles.header}  ${scroll ? styles.sticky : undefined}`}
+        >
+          <ul
+            className={`${styles.navigation} ${
+              active ? styles.active : undefined
+            }`}
+          >
             <li className={styles.left}>
               <div className={styles.flex}>
                 <a onClick={goStudentBoard}>Student</a>
@@ -40,10 +62,28 @@ const MenuBar = () => {
                 <div className={`${styles.dot} ${styles.right}`}></div>
               </div>
             </li>
+            <li className={`${styles.left} ${styles.formenu}`}>
+              <div className={styles.flex}>
+                <div className={`${styles.dot} ${styles.left}`}></div>
+                <a onClick={goExamBoard}>Exam</a>
+              </div>
+            </li>
+            <li className={`${styles.left} ${styles.formenu}`}>
+              <div className={styles.flex}>
+                <div className={`${styles.dot} ${styles.left}`}></div>
+                <a>Sign up</a>
+              </div>
+            </li>
           </ul>
           <a onClick={goHome} className={styles.logo}>
             학생<span>.</span>
           </a>
+          <div
+            className={`${styles.menuToggle} ${
+              active ? styles.active : undefined
+            }`}
+            onClick={toggleMenu}
+          ></div>
           <ul className={styles.navigation}>
             <li className={styles.right}>
               <div className={styles.flex}>

dc49526452f28afb4551a6e735b9369c05d0ea04
Long Thanh Bach <bachthanhlong0821@gmail.com>
2024-10-15 14:23:45 +0700

    refactor code

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
 };
 
 module.exports = nextConfig;
---
+++ pages/student/detail/component/About.tsx
@@ -0,0 +1,8 @@
+import { Box } from 'ui-components/General/Box';
+import React from 'react';
+
+const About = () => {
+  return <Box h="100%">About</Box>;
+};
+
+export default About;
